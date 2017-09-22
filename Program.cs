5daaaf1a8d49727c366e4c27c7f7e7c9b0d1e3ae
Alexander <Alexander@gegitech.net>
2017-09-22 17:49:37 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sharpire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                return;
+            }
+            String server = args[0];
+            String stagingKey = args[1];
+            String language = args[2];
+
+            (new EmpireStager(server, stagingKey, language)).execute();
+        }
+    }
+}
